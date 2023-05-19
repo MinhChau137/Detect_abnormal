eb8cf170c6927594ba9592109804e8d89276ccba
MinhChau137 <chau.ntm137@gmail.com>
2023-05-19 19:55:47 +0200

    fix not connect but have data

--- main.py
+++ main.py
@@ -40,33 +40,29 @@ def terminate_process():
 
 def detect_abnormal(ip_server: str, port_server: int,time_to_detect: int, time_sleep: int):
     loaded_model = load_model('knn_3.8')
-    if time_to_detect == 1:
-        data = pd.read_csv('./data/data_add.csv').tail(1) 
-        predict = predict_model(loaded_model, data=data)
-        print(predict.iloc[:,-2])
-    else: 
-        s = socket.socket(socket.AF_INET,
-                  socket.SOCK_STREAM)
-        s.connect((ip_server, port_server))
-        start_time = time.monotonic()
-        while True:
+
+    s = socket.socket(socket.AF_INET,
+                socket.SOCK_STREAM)
+    s.connect((ip_server, port_server))
+    start_time = time.monotonic()
+    while True:
+        msg = s.recv(8192)
+        while msg:
+            msg_recv = msg.decode()
+            with open('./data/data_add.csv', 'a') as the_file:
+                the_file.write(msg_recv)
+                
+            data = pd.read_csv('./data/data_add.csv').tail(1)
+            
+            predict = predict_model(loaded_model, data=data)
+            print(predict.iloc[:,-2])                
+            time.sleep(time_sleep)
+            
+            if time.monotonic() - start_time > time_to_detect or IS_TERMINATE:
+                # disconnect the client
+                s.close()
+                return False
             msg = s.recv(8192)
-            while msg:
-                msg_recv = msg.decode()
-                with open('./data/data_add.csv', 'a') as the_file:
-                    the_file.write(msg_recv)
-                    
-                data = pd.read_csv('./data/data_add.csv').tail(1)
-                
-                predict = predict_model(loaded_model, data=data)
-                print(predict.iloc[:,-2])                
-                time.sleep(time_sleep)
-                
-                if time.monotonic() - start_time > time_to_detect or IS_TERMINATE:
-                    # disconnect the client
-                    s.close()
-                    return False
-                msg = s.recv(8192)
 
 
 
